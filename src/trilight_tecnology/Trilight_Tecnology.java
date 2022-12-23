05d2c150b27b304cc02763cb390ea1996ca7104e
Isaias RM <112596331+RedoMarshallX@users.noreply.github.com>
2022-12-23 17:08:25 -0600

    Implementación de menú (BETA - v 1.0.0)
    Esto era lo que llevaba del menú para la terminal (contando con un usuario y una contraseña escritos en el código)

--- src/trilight_tecnology/Trilight_Tecnology.java
+++ src/trilight_tecnology/Trilight_Tecnology.java
@@ -10,6 +10,8 @@ import java.util.HashMap;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  *
@@ -22,6 +24,64 @@ public class Trilight_Tecnology {
      */
     public static void main(String[] args) {
         Generar x = new Generar("./src/db/alumnos.csv","");
+        
+        //Variables para el MENU PRINCIPAL
+        Scanner opt = new Scanner(System.in);
+        boolean salir = false;
+        int opcion;
+        
+        //Variables para el USUARIO
+        Scanner ingreso = new Scanner(System.in);
+        String usuario, contraseña;
+        
+        //Menu de acceso
+        System.out.println("Ingrese su nombre de usuario:");
+        usuario = ingreso.nextLine();
+        System.out.println("Ingrese su contraseña:");
+        contraseña = ingreso.nextLine();
+        
+        if(usuario.equals("AdminTT") && contraseña.equals("POO01")) {
+            System.out.println("¡Bienvenido administrador TT!");
+            
+            //Menu principal del programa
+            while(!salir){
+                System.out.println("\n BASE DE REGISTROS ACADÉMICOS (v.1.0.0)");
+                System.out.println("1. Consultar registros");
+                System.out.println("2. Consultar datos personales");
+                System.out.println("3. Consultar numero de inscripción");
+                System.out.println("4. Salir");
+                System.out.println("Escoja una opción: ");
+                
+                try{
+                opcion = opt.nextInt();
+                
+                switch(opcion)
+                {
+                    case 1 -> {
+                        System.out.println("CONSULTA DE REGISTROS ACADÉMICOS");
+                        System.out.println("Ingrese el número de cuenta:");
+                        }
+                    case 2 -> {
+                        System.out.println("CONSULTA DE DATOS PERSONALES");
+                        System.out.println("Ingrese el número de cuenta:");
+                        }
+                    case 3 -> {
+                        System.out.println("CONSULTA DE NÚMERO DE INSCRIPCIÓN");
+                        System.out.println("Ingrese el número de cuenta:");
+                        }
+                    case 4 -> {
+                        salir = true;
+                        }
+                    default -> System.out.println("La opción seleccionada no existe");
+                }
+                }catch(InputMismatchException e){
+                    System.out.println("Debes introducir un número");
+                    opt.next();
+                }
+            }
+        }else{
+            System.out.println("El usuario y/o la contraseña son incorrectos");
+        }
     }
     
 }

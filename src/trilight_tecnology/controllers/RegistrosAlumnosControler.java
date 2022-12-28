eff3abfbac507c7c126756adbf5950ab047ac25c
Anvil <martinezerickaguilar@comunidad.unam.mx>
2022-12-28 17:02:36 -0600

    Creacion del controlador de alumnos CRUD

---
+++ src/trilight_tecnology/controllers/RegistrosAlumnosControler.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trilight_tecnology.controllers;
+
+/**
+ * CRUD de los alumnos
+ * @author Anvil
+ */
+public class RegistrosAlumnosControler {
+    
+}

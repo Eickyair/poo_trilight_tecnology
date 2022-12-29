a06e8ad5524255a8c2287d705a9f973a176a968b
Anvil <martinezerickaguilar@comunidad.unam.mx>
2022-12-29 08:19:49 -0600

    Prueba de eliminacion de registro en un Csv TestCsv.java

---
+++ src/trilight_tecnology/controllers/TestCsv.java
@@ -0,0 +1,23 @@
+package trilight_tecnology.controllers;
+
+
+import trilight_tecnology.controllers.Csv;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Anvil
+ */
+public class TestCsv {
+    public void TesEliminarRegistro(){
+        Csv csv = new Csv("./db/mainAlu.csv");
+        Boolean res = csv.elimarRegistro(0);
+        if(res){
+            System.out.println("Borrado correctamente");
+        }else System.out.println("No se pudo eliminar");
+    }
+}

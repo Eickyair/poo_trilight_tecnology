fbefc2178bf396fac1dd34d4a70f54252b1e6036
Milanesus <you@example.com>
2022-12-28 20:25:46 -0600

    Creacion del objeto HistorialAcademico/Semestre/Materia

---
+++ src/trilight_tecnology/models/HistorialAcademico.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trilight_tecnology.models;
+
+import java.util.Hashtable;
+
+
+/**
+ *
+ * @author Milanesus
+ */
+public class HistorialAcademico {
+    public Integer idAlumno;
+    public double promedio;
+    public Hashtable<Integer, Semestre> semestres;
+
+    public HistorialAcademico(Integer idAlumno, double promedio, Hashtable<Integer, Semestre> semestres) {
+        this.idAlumno = idAlumno;
+        this.promedio = promedio;
+        this.semestres = semestres;
+    }
+    
+    
+}
---
+++ src/trilight_tecnology/models/Materia.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trilight_tecnology.models;
+
+/**
+ *
+ * @author Milanesus
+ */
+public class Materia {
+    public Integer clave;
+    public String nombre;
+    public Integer creditos;
+    public Integer idAlumno;
+    public double calificacion;
+
+    public Materia(Integer clave, String nombre, Integer creditos, Integer idAlumno, double calificacion) {
+        this.clave = clave;
+        this.nombre = nombre;
+        this.creditos = creditos;
+        this.idAlumno = idAlumno;
+        this.calificacion = calificacion;
+    }
+    
+}

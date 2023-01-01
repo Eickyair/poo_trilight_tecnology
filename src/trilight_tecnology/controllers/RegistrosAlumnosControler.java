a62402238dcbe4a28d03544ec30c956e89e12336
Anvil <martinezerickaguilar@comunidad.unam.mx>
2022-12-31 22:20:23 -0600

    Uso dentro de los metodos de RegistrosAlumnosControler.java del objeto Casteador

--- src/trilight_tecnology/controllers/RegistrosAlumnosControler.java
+++ src/trilight_tecnology/controllers/RegistrosAlumnosControler.java
@@ -5,7 +5,7 @@
 package trilight_tecnology.controllers;
 
 import java.util.Arrays;
-import java.util.StringTokenizer;
+
 import trilight_tecnology.models.RegistroAlumno;
 import trilight_tecnology.views.RegistrosAlumnosView;
 
@@ -20,9 +20,10 @@ public class RegistrosAlumnosControler {
     public RegistrosAlumnosControler(){
         Csv  csv = new Csv("./db/mainAlu.csv");
         String[] todosLosRegistros = csv.getAllRecords();
+        Casteador cast = new Casteador();
         alumnos = new RegistroAlumno[todosLosRegistros.length];
         for (int i = 0; i < alumnos.length; i++) {
-            parseRegistroAlumno(todosLosRegistros[i]);
+            alumno = cast.castRegistroAlumno(todosLosRegistros[i]);
             alumnos[i] = alumno;
         }
     }
@@ -36,35 +37,13 @@ public class RegistrosAlumnosControler {
         RegistrosAlumnosView view = new RegistrosAlumnosView(alumnos);
         view.mostrarEnSlides(20);
     }
-    private void parseRegistroAlumno(String rec){
-        alumno = new RegistroAlumno();
-        int i=0;
-        StringTokenizer tok = new StringTokenizer(rec,",");
-        while(tok.hasMoreElements()){
-            switch (i) {
-                case 0:
-                    alumno.idAlumno = Integer.parseInt(tok.nextToken());                    
-                    break;
-                case 1:
-                    alumno.nombre = tok.nextToken();
-                    break;
-                case 2:
-                    alumno.apellido = tok.nextToken();
-                    break;
-                case 3:
-                    alumno.direccion = tok.nextToken();
-                    break;
-                case 4:
-                    alumno.edad = Integer.parseInt(tok.nextToken());
-            }
-            i++;
-        }
-    }
+
     public Boolean borrarAlumno(Integer id){
         Csv csv = new Csv("./db/mainAlu.csv");
         String[] data = csv.getAllRecords();
+        Casteador caster = new Casteador();
         for (int i = 0; i < data.length; i++) {
-            parseRegistroAlumno(data[i]);
+            alumno = caster.castRegistroAlumno(data[i]);
             if(alumno.idAlumno == id){
                return csv.elimarRegistro(i);
             }
@@ -74,10 +53,21 @@ public class RegistrosAlumnosControler {
     public RegistroAlumno busqueda(Integer idAlumno){
         Csv csv = new Csv("./db/mainAlu.csv");
         String[] alumnos = csv.getAllRecords();
+        Casteador cast = new Casteador();
         for (int renglon = 0; renglon < alumnos.length; renglon++) {
-            parseRegistroAlumno(alumnos[renglon]);
+            alumno = cast.castRegistroAlumno(alumnos[renglon]);
             if(alumno.idAlumno==idAlumno)return alumno;
         }
         return null;
+    }
+
+    public Boolean guardarAlumno(RegistroAlumno alumno){
+        Csv csv = new Csv("./db/mainAlu.csv");
+        String record = alumno.recordDb();
+        return csv.insertarUnRegistro(record);
+    }
+    public Boolean actualizarAlumno(RegistroAlumno nuevo){
+        borrarAlumno(nuevo.idAlumno);
+        return guardarAlumno(nuevo);
     }
 }

936400de86b3fb37da26716f648ebe6ecd80d3c8
Anvil <martinezerickaguilar@comunidad.unam.mx>
2023-01-04 21:28:13 -0600

    Eliminacion de importaciones inecesarias

--- src/trilight_tecnology/controllers/Csv.java
+++ src/trilight_tecnology/controllers/Csv.java
@@ -12,7 +12,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
--- src/trilight_tecnology/controllers/NumInscriControler.java
+++ src/trilight_tecnology/controllers/NumInscriControler.java
@@ -2,7 +2,6 @@ package trilight_tecnology.controllers;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
